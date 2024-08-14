f2acef223f271a801542b97bab27c5a30bdaee59
scyppan <adam@scyppan.com>
2024-08-14 09:31:45 -0500

    Update objectiveswindow.js
    Completed objectives window functions.

--- objectiveswindow.js
+++ objectiveswindow.js
@@ -1,3 +1,24 @@
+const objectivesOptions = [
+    'REACH_GROUPS',
+    'REACH_QUARTER_FINALS',
+    'REACH_SEMI_FINALS',
+    'CHAMPION',
+    'REACH_FINALS',
+    'REACH_KNOCKOUT',
+    'PRIMARY_REGIONAL_CUP_SLOT',
+    'ANY_REGIONAL_CUP_SLOT',
+    'MID_TABLE',
+    'AVOID_LOWLY_FINISH',
+    'FIGHT_FOR_TITLE',
+    'HIGH_FINISH',
+    'REACH_ROUND_OF_16',
+    'SECONDARY_REGIONAL_CUP_SLOT',
+    'AVOID_RELEGATION',
+    'PROMOTION',
+    'FIGHT_FOR_PROMOTION',
+    'REACH_PLAYOFFS'
+];
+
 function createObjectivesDiv(id) {
     // Get objectives data for the specified id
     const objectivesData = getDataForId('objectives', id);
@@ -9,14 +30,6 @@ function createObjectivesDiv(id) {
     const header = document.createElement('h2');
     header.innerHTML = `Objectives - ${getCompetitionNameById(id)}`;
     div.appendChild(header);
-
-    if (objectivesData.length === 0) {
-        const objectivesParagraph = document.createElement('p');
-        objectivesParagraph.textContent = 'No objectives';
-        objectivesParagraph.style.fontSize = "12px";
-        header.appendChild(objectivesParagraph);
-        return div;
-    }
 
     // Create table to display objectives data
     const table = document.createElement('table');
@@ -33,92 +46,118 @@ function createObjectivesDiv(id) {
     table.appendChild(thead);
 
     const tbody = document.createElement('tbody');
+    table.appendChild(tbody);
 
-    objectivesData.forEach(entry => {
-        const row = document.createElement('tr');
-        row.dataset.id = entry.id; // Set data-id attribute
-        row.dataset.type = 'objectives'; // Set data-type attribute
-
-        const createCellWithInput = (type, value, key) => {
-            const cell = document.createElement('td');
-            let input;
-
-            if (type === 'select') {
-                input = document.createElement('select');
-                const objectivesOptions = [
-                    'REACH_GROUPS',
-                    'REACH_QUARTER_FINALS',
-                    'REACH_SEMI_FINALS',
-                    'CHAMPION',
-                    'REACH_FINALS',
-                    'REACH_KNOCKOUT',
-                    'PRIMARY_REGIONAL_CUP_SLOT',
-                    'ANY_REGIONAL_CUP_SLOT',
-                    'MID_TABLE',
-                    'AVOID_LOWLY_FINISH',
-                    'FIGHT_FOR_TITLE',
-                    'HIGH_FINISH',
-                    'REACH_ROUND_OF_16',
-                    'SECONDARY_REGIONAL_CUP_SLOT',
-                    'AVOID_RELEGATION',
-                    'PROMOTION',
-                    'FIGHT_FOR_PROMOTION',
-                    'REACH_PLAYOFFS'
-                ];
-
-                objectivesOptions.forEach(optionValue => {
-                    const option = document.createElement('option');
-                    option.value = optionValue;
-                    option.textContent = optionValue;
-                    if (optionValue === value) {
-                        option.selected = true;
-                    }
-                    input.appendChild(option);
-                });
-            } else {
-                input = document.createElement('input');
-                input.type = type;
-                input.value = value;
-            }
-
-            input.classList.add('tablevalue-input');
-            input.dataset.key = key;
-            input.dataset.context = 'objectives';
-            cell.appendChild(input);
-            return { cell, input };
-        };
-
-        const { cell: objectiveCell, input: objectiveInput } = createCellWithInput('select', entry.objective, 'objective');
-        const { cell: valueCell, input: valueInput } = createCellWithInput('number', entry.value, 'value');
-
-        // Event listener for objective change
-        objectiveInput.addEventListener('change', function () {
-            updateObjectivesData(entry.id, 'objective', objectiveInput.value);
-            entry.objective = objectiveInput.value;  // Update the local variable to reflect the change
-        });
-
-        // Event listener for value change
-        valueInput.addEventListener('change', function () {
-            if (!valueInput.value) {
-                // If value is deleted (empty), remove the row and update the data
-                row.remove(); // Remove the row from the table
-                deleteObjectiveData(entry.id); // Remove the corresponding data entry
-            } else {
-                updateObjectivesData(entry.id, 'value', valueInput.value);
-                entry.value = valueInput.value;  // Update the local variable to reflect the change
-            }
-        });
-
-        row.appendChild(objectiveCell);
-        row.appendChild(valueCell);
-
-        tbody.appendChild(row);
+    // Create button for adding new objectives
+    const createButton = document.createElement('button');
+    createButton.textContent = 'Create New Objective';
+    createButton.addEventListener('click', function () {
+        let newObjective = createNewObjectiveData(id);
+        table.classList.remove('hidden');
+        const noDataMessage = div.querySelector('.no-data-message');
+        if (noDataMessage) {
+            noDataMessage.remove();
+        }
+        addObjectiveRow(newObjective, tbody);
     });
 
-    table.appendChild(tbody);
     div.appendChild(table);
+    div.appendChild(createButton); // Append the button after the table
+
+    objectivesData.forEach(entry => {
+        addObjectiveRow(entry, tbody); // Ensure tbody is passed correctly
+    });
+
+    // Check for empty objectives data
+    if (objectivesData.length === 0) {
+        table.classList.add('hidden'); // Hide the table if there's no data
+        const objectivesParagraph = document.createElement('p');
+        objectivesParagraph.textContent = 'No objectives';
+        objectivesParagraph.classList.add('no-data-message'); // Add a class for easy targeting later
+        objectivesParagraph.style.fontSize = "12px";
+        div.appendChild(objectivesParagraph); // Append the message to the main div
+    }
 
     return div;
+}
+
+function createNewObjectiveData(id) {
+    // Create the new objective entry
+    let newObjective = {
+        id: id,
+        objective: 'REACH_GROUPS', // Default objective
+        value: 0 // Default value
+    };
+
+    data['objectives'].push(newObjective);
+
+    return newObjective;
+}
+
+function addObjectiveRow(entry, tbody) {
+    if (!tbody) {
+        console.error('No table body found to add the row.');
+        return;
+    }
+
+    const row = document.createElement('tr');
+    row.dataset.id = entry.id; // Set data-id attribute
+    row.dataset.type = 'objectives'; // Set data-type attribute
+
+    const createCellWithInput = (type, value, key) => {
+        const cell = document.createElement('td');
+        let input;
+
+        if (type === 'select') {
+            input = document.createElement('select');
+
+            objectivesOptions.forEach(optionValue => {
+                const option = document.createElement('option');
+                option.value = optionValue;
+                option.textContent = optionValue;
+                if (optionValue === value) {
+                    option.selected = true;
+                }
+                input.appendChild(option);
+            });
+        } else {
+            input = document.createElement('input');
+            input.type = type;
+            input.value = value;
+        }
+
+        input.classList.add('tablevalue-input');
+        input.dataset.key = key;
+        input.dataset.context = 'objectives';
+        cell.appendChild(input);
+        return { cell, input };
+    };
+
+    const { cell: objectiveCell, input: objectiveInput } = createCellWithInput('select', entry.objective, 'objective');
+    const { cell: valueCell, input: valueInput } = createCellWithInput('number', entry.value, 'value');
+
+    // Event listener for objective change
+    objectiveInput.addEventListener('change', function () {
+        updateObjectivesData(entry.id, 'objective', objectiveInput.value);
+        entry.objective = objectiveInput.value;  // Update the local variable to reflect the change
+    });
+
+    // Event listener for value change
+    valueInput.addEventListener('change', function () {
+        if (!valueInput.value) {
+            // If value is deleted (empty), remove the row and update the data
+            row.remove(); // Remove the row from the table
+            deleteObjectiveData(entry.id); // Remove the corresponding data entry
+        } else {
+            updateObjectivesData(entry.id, 'value', valueInput.value);
+            entry.value = valueInput.value;  // Update the local variable to reflect the change
+        }
+    });
+
+    row.appendChild(objectiveCell);
+    row.appendChild(valueCell);
+
+    tbody.appendChild(row); // Add the new row to the table body
 }
 
 function updateObjectivesData(id, key, value) {
